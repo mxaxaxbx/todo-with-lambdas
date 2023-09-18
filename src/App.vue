758853a87c51d50831652c06847a660dddb781e9
Miguel Angel Arenas <marenas@ikatechsolutions.com>
2023-09-18 10:23:33 -0500

    The form to creation was added

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,18 @@
       <v-form validate-on="submit lazy" @submit.prevent="submit">
         <v-container fluid >
           <v-row justify="center" >
+            <!-- show posible errors -->
+            <v-col cols="12" md="12">
+              <v-alert type="error" v-if="error" dismissible>
+                {{ error }}
+              </v-alert>
+            </v-col>
             <v-col cols="12" md="10">
-              <v-text-field clearable variant="outlined">
+              <v-text-field clearable variant="outlined" v-model="task">
               </v-text-field>
             </v-col>
             <v-col cols="2" md="2">
-              <v-btn icon size="small" color="blue" type="submit">
+              <v-btn icon size="small" color="blue" type="submit" :loading="loading">
                 <v-icon>mdi-send</v-icon>
               </v-btn>
             </v-col>
@@ -22,11 +28,34 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        valid: false,
+import { httpClient } from './http-client';
+
+export default {
+  data () {
+    return {
+      valid: false,
+      task: '',
+      loading: false,
+      error: '',
+    }
+  },
+
+  methods: {
+    async submit () {
+      this.loading = true;
+      try {
+        const res = await httpClient.post('', {
+          task: this.task,
+        });
+        this.task = '';
+        console.log('res',res);
+      } catch (error) {
+        console.log(error);
+        this.error = error.message;
+      } finally {
+        this.loading = false;
       }
     },
   }
+}
 </script>
